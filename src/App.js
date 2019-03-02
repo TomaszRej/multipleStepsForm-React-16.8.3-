f7d92f4944e24835a1b277ed0a7f5795060e166e
Tomasz Rej <tomaszrej@iMac-Tomasz.local>
2019-03-02 16:13:45 +0100

    pagination added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { Button } from './components/Button';
 
 export class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       step: 1,
-      email: '',
-      name: ""
+      data: {
+        email: {
+          value: ''
+        },
+        name: {
+          value: ''
+        },
+        city: {
+          value: ''
+        }
+      }
+
     }
 
 
@@ -18,68 +28,151 @@ export class App extends React.Component {
 
   prev = (e) => {
     e.preventDefault();
-    this.setState( prevState => {
+    if (this.state.step === 1) {
+      return;
+    }
+    this.setState(prevState => {
       console.log(prevState);
-      const step =  prevState.step - 1;
-      return{
-      step: step
+      const step = prevState.step - 1;
+      return {
+        step: step
       };
-    })      
+    })
   }
 
   next = (e) => {
     e.preventDefault();
-    this.setState( prevState => {
+    if (this.state.step === 4) {
+      return;
+    }
+    this.setState(prevState => {
       console.log(prevState);
-      const step =  prevState.step + 1;
-      return{
-      step: step
+      const step = prevState.step + 1;
+      return {
+        step: step
       };
-    })   
+    }, () => {
+      console.log(this.state.step);
+    })
 
   }
 
-  handleChange = (e)=> {
-  console.log(e.target);
-    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-   const name = e.target.name;
+  goTo = (e) => {
+    // e.target.getAttribute('data-id');
+    // e.target.dataset.id;
+    e.preventDefault();
+    const id = parseInt(e.target.dataset.id);
+
 
     this.setState({
-      [name]: value
+      step: id
+    }, () => {
+      console.log(this.state.step);
+    })
+  }
+
+  handleChange = (e) => {
+    console.log(e.target);
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    const name = e.target.name;
+
+    this.setState(state => {
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [name]: {
+            value: value
+          }
+
+        }
+      }
+
     });
   }
 
   renderForm = () => {
-
+    if (!this.state.step) {
+      return;
+    }
     switch (this.state.step) {
       case 1:
         return (
           <>
-          <TextInput name="email" label="Email address: " value={ this.state.email } onChange={() => this.handleChange} placeholder="enter email" info="We'll never share your email with anyone else." />
-          <Button title="prev" onClick={() => this.prev}/>
-           <Button title="next" onClick={() => this.next} /> 
-        </>
+            <TextInput name="email" label="Email address: " value={this.state.data.email.value} onChange={() => this.handleChange} placeholder="enter email" info="We'll never share your email with anyone else." />
+            <div className="d-flex">
+              <div className=" d-flex flex-fill pr-2" >
+                <Button title="prev" onClick={() => this.prev} />
+              </div>
+              <div className="d-flex flex-fill pl-2">
+                <Button title="next" onClick={() => this.next} />
+              </div>
+            </div>
+          </>
         )
       case 2:
         return (
           <>
-            <TextInput name="name" label="name: " value={ this.state.name } onChange={() => this.handleChange} placeholder="name" info="info test" />
-            <Button title="prev" onClick={() => this.prev}/>
-             <Button title="next" onClick={() => this.next} /> 
-
-
+            <TextInput name="name" label="name: " value={this.state.data.name.value} onChange={() => this.handleChange} placeholder="name" />
+            <div className="d-flex">
+              <div className=" d-flex flex-fill pr-2" >
+                <Button title="prev" onClick={() => this.prev} />
+              </div>
+              <div className="d-flex flex-fill pl-2">
+                <Button title="next" onClick={() => this.next} />
+              </div>
+            </div>
           </>
+        )
+      case 3:
+        return (
+          <>
+            <TextInput name="city" label="city: " value={this.state.data.city.value} onChange={() => this.handleChange} placeholder="city" />
+            <div className="d-flex">
+              <div className=" d-flex flex-fill pr-2" >
+                <Button title="prev" onClick={() => this.prev} />
+              </div>
+              <div className="d-flex flex-fill pl-2">
+                <Button title="Submit" onClick={() => this.next} />
+              </div>
+            </div>
+          </>
+        )
+      case 4:
+        return (
+          <h3>Thank You for filling out the form.</h3>
         )
       default:
         console.log('xxxx');
     }
 
   }
+  renderPagination = (goTo) => {
+    const pagination = this.state.step !== 4 ? (
+      <nav aria-label="Page navigation example" className="pt-3 d-flex justify-content-center" >
+        <ul className="pagination">
+
+          <li className={this.state.step === 1 ? "page-item active" : "page-item"}><a className="page-link" onClick={(e) => goTo(e)} data-id="1">1</a></li>
+          <li className={this.state.step === 2 ? "page-item active" : "page-item"}><a className="page-link" onClick={(e) => goTo(e)} data-id="2">2</a></li>
+          <li className={this.state.step === 3 ? "page-item active" : "page-item"}><a className="page-link" onClick={(e) => goTo(e)} data-id="3">3</a></li>
+        </ul>
+      </nav>
+    ) : null;
+
+    return pagination;
+  }
   render() {
     return (
-      <Form>
-        {this.renderForm()}
-      </Form>
+      <div className="container">
+        <div className="card">
+          <div className="card-header" >Form</div>
+          <Form>
+            {this.renderForm()}
+            {this.renderPagination(this.goTo)}
+          </Form>
+
+        </div>
+      </div>
     )
   }
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,6 @@ import React,{useState, useEffect} from 'react';
 export const Button = props => {
 
     return (
-        <button  onClick={typeof props.onClick === "function"  ? props.onClick() : null} className="btn btn-primary">{props.title}</button>
+        <button  onClick={typeof props.onClick === "function"  ? props.onClick() : null} className="btn btn-primary flex-fill" >{props.title}</button>
     )
 }
